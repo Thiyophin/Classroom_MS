b9a1cca9c4400c00c107ab35e5f011060350c6ad
Thiyophin <thiyophinj99@gmail.com>
2021-01-12 00:33:42 +0530

    heruko deploy and mongodb to atlas

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state={
     db:null
 }
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017'
+    const url=process.env.MONGO_URL || 'mongodb://localhost:27017/dbCluster'
     const dbname='classroom'
 
     mongoClient.connect(url,(err,data)=>{
