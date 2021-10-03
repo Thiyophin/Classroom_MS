13c3209574ba522e13e792fedd105e6aa36832e6
Thiyophin Johnson <thiyophinj99@gmail.com>
2021-10-03 16:33:51 +0530

    some fix

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,10 @@ const state = {
   db: null,
 };
 module.exports.connect = function (done) {
-  const url = "mongodb://localhost:27017";
+  const url = process.env.MONGO_URL;
   const dbname = "classroom";
 
-  mongoClient.connect(url, (err, data) => {
+  mongoClient.connect(url {useUnifiedTopology:true},(err, data) => {
     if (err) return done(err);
     state.db = data.db(dbname);
     done();
